7eac1931f3a7de789b81bd1da1e6579b57bedc95
gds90 <you@example.com>
2024-01-15 10:34:02 +0100

    Changed API endpoint language

--- src/store.js
+++ src/store.js
@@ -5,7 +5,7 @@ export const store = reactive({
     endpoint: 'https://api.themoviedb.org/3',
     searchFilm: 'https://api.themoviedb.org/3/search/movie',
     searchSerie: 'https://api.themoviedb.org/3/search/tv',
-    apiKey: '?api_key=90b44e3da2719a7753a92b81c9a32639',
+    apiKey: '?api_key=90b44e3da2719a7753a92b81c9a32639&language=it',
     filmsArray: [],
     seriesArray: [],
     search: '',
